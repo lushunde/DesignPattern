7da1faea5ae540249b8a29625014a8c2100c01d8
bellus <lushunde321@163.com>
2020-03-26 00:16:49 +0800

    开闭原则

---
+++ src/com/lushunde/desingn/opc/notimprove/Client.java
@@ -0,0 +1,22 @@
+package com.lushunde.desingn.opc.notimprove;
+
+/**
+ * 以图书销售为例
+ * 
+ * 原来： 直接原价销售
+ * 新需求： 运价打8折出售
+ * 		1. 修改接口
+ * 		2. 修改实现类
+ * 	改进：通过扩展新实现
+ * @author bellus
+ * 	本例修改实现类
+ */
+
+public class Client {
+	public static void main(String[] args) {
+
+		IBook novel = new NovelBook("笑傲江湖", 100, "金庸");
+		System.out.println("书籍名字：" + novel.getName() + "书籍作者：" + novel.getAuthor() + "书籍价格：" + novel.getPrice());
+	}
+
+}
---
+++ src/com/lushunde/desingn/opc/notimprove/IBook.java
@@ -0,0 +1,17 @@
+package com.lushunde.desingn.opc.notimprove;
+
+/**
+ * Êé¼®½Ó¿Ú
+ * @author bellus
+ *
+ */
+
+public interface IBook {
+
+	public String getName();
+
+	public Double getPrice();
+
+	public String getAuthor();
+
+}
